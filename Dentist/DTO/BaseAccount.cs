1e549a80df58ffa7ae5d26e2e2d0de0b6ab664e6
LAPTOP <LAPTOP@DESKTOP-L515TMB>
2023-09-24 16:55:02 +0300

    add-gister-and-login

---
+++ Dentist/DTO/BaseAccount.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dentist.DTO
+{
+    public class BaseAccount
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ Dentist/Services/AppUser.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dentist.Services
+{
+    public class AppUser:IdentityUser
+    {
+        [MaxLength(50)]
+        public string firstName { get; set; }
+
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+
+        [MaxLength(100)]
+        public string Address { get; set; }
+
+        //public virtual List<Order>? Order { get; set; } = new List<Order>();
+
+        //public virtual Review? Review { get; set; }
+        //public virtual ShopingCart? ShopingCart { get; set; }
+        public override string ToString()
+        {
+            return this.firstName + this.LastName + this.Address;
+        }
+    }
+}
